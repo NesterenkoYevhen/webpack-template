1fef74134df77faf3337ed2094ebf88054d44c55
Nesterenko Yevhen <enesterenko76@gmail.com>
2025-06-14 14:45:08 +0300

    Third commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import LOGO from './logo.svg'
 import IMAGE from './react.png'
 
 export const App = () => {
-  const name = "Vishwas";
+  const name = 'Vishwasd'
 
   return (
     <>
       <h1>
-        Edited React TypeScript Webpack Starter Template -{' '}
-        {process.env.NODE_ENV} {process.env.name}
+        Edited React TypeScript Webpack Starter Template {name} - {process.env.NODE_ENV}{' '}
+        {process.env.name}
       </h1>
       <img src={IMAGE} alt="React Logo" width={300} height={200} />
       <img src={LOGO} alt="React Logo" width={300} />
